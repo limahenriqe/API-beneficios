be0a21f275c61099a63a755a0dc7b3a46b57b635
Henrique Lima <henriqueslima.08@gmail.com>
2026-07-15 18:21:32 -0300

    Subindo beneficios de funcionarios

---
+++ models/Beneficio.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Beneficio = sequelize.define("Beneficio",{
+    id:{
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    funcionarioId:{
+        type:DataTypes.INTEGER,
+        allowNull:false
+    },
+    valeAlimentacao:{
+        type:DataTypes.DECIMAL(10,2),
+        allowNull:false
+    },
+    valeTransporte:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false
+    },
+    planoSaude:{
+        type:DataTypes.DECIMAL(10,2),
+        allowNull:false
+    }
+})
+
+module.exports = Beneficio
